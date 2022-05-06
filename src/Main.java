2f2e49ca7be0edb833874789cbf33085a3b3dcef
VladislavGV <0377722@mail.ru>
2022-05-06 10:32:21 +0300

    Cats example

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        Cat[] cats = new Cat[4];
+        cats[0] = new Cat("Tomas");
+        cats[1] = new Cat("Begemot");
+        cats[2] = new Cat("Fillip Maximovich");
+        cats[3] = new Cat("Kotya");
 
+       // cats[1] = null;
+
+
+        System.out.println(Arrays.toString(cats));
     }
+
+
 }
